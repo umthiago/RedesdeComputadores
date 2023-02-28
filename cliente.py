8ee8e1858499bb3ea8b623927f1e15a3e50955c9
Thiago <124899004+umthiago@users.noreply.github.com>
2023-02-28 15:27:51 -0300

    Create cliente.py

---
+++ cliente.py
@@ -0,0 +1,18 @@
+
+
+import socket
+
+port = 10500
+dest = '192.168.246.37'
+##msg = 'Busquem conhecimento. Atte Et Bilu.'
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print(f'=== Conectando ao servidor {dest} : {port} ===')
+client.connect((dest,port))
+
+##cria um input pro cliente mandar mensagem pro servidor
+msg = input(': ')
+client.send(msg.encode())
+
+client.close
