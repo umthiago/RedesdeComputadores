3d28b78970045f5d48b9228bbd097a7a8bdcbb41
Thiago <124899004+umthiago@users.noreply.github.com>
2023-02-28 16:15:14 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -3,7 +3,7 @@
 import socket
 
 port = 10500
-dest = '192.168.246.37'
+dest = 'localhost'
 ##msg = 'Busquem conhecimento. Atte Et Bilu.'
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,7 +12,12 @@ print(f'=== Conectando ao servidor {dest} : {port} ===')
 client.connect((dest,port))
 
 ##cria um input pro cliente mandar mensagem pro servidor
-msg = input(': ')
-client.send(msg.encode())
+while True:
+    msg = input(': ')
+    client.send(msg.encode())
 
-client.close
+    msg = client.recv(4096)
+    print(f'Servidor: {msg.decode()}')
+
+    
+    client.close
