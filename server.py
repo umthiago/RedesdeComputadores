364c4731cec34df8e01791afa8918d775d134922
Thiago <124899004+umthiago@users.noreply.github.com>
2023-02-28 15:26:56 -0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+port = 10500
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('0.0.0.0', port))
+server.listen()
+while True:
+    print(f"===  Servidor aguardando conexôes na porta {port} ===")
+    conn, addr = server.accept()
+    print(f'Conexão recebida de {addr}')
+
+    ##recebe os dados enviados pelo cliente.
+    data = conn.recv(4096)
+    print(f'Recebido: {data.decode()}')
+
+    ##encerra a conexão
+    conn.close
