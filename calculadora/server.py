9ee32055088c45f06fd9fb29a1d728532f541969
Thiago <124899004+umthiago@users.noreply.github.com>
2023-03-07 15:29:53 -0300

    Update server.py

--- calculadora/server.py
+++ calculadora/server.py
@@ -1,23 +1,45 @@
 import socket
+
 port = 10500
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 server.bind(('0.0.0.0', port))
 server.listen()
 
+
 print(f"===  Servidor aguardando conexôes na porta {port} ===")
 conn, addr = server.accept()
+
 print(f'Conexão recebida de {addr}')
-
+    
 while True:
-    ##recebe os dados enviados pelo cliente.
+    
     data = conn.recv(4096)
-    print(f'Cliente: {data.decode()}')
-
+    msgclient = data.decode().split()
     
-    msg = input(': ')
+    if msgclient[1] == "+":
+        
+        resultado = int(msgclient[0]) + int(msgclient[2])
+        
+    elif msgclient[1] == "-":
+        
+        resultado = int(msgclient[0]) - int(msgclient[2])
+        
+    elif msgclient[1] == "*":
+        
+        resultado = int(msgclient[0]) * int(msgclient[2])
+        
+    elif msgclient[1] == "/":
+        
+        resultado = int(msgclient[0]) / int(msgclient[2])
+    
+    print(resultado)
+    
+
+    msg = str(resultado)
+    
     conn.send(msg.encode())
-    ##encerra a conexão
-
-
-    conn.close
     
+    
+    conn.close #encerra conexão
